ec5c51dff5d89cdd38041f11fe0751e7a7cfc810
Ardakaniz <ardakaniz@e.email>
2022-05-07 22:50:46 +0200

    Fix (?) fixed lattice charges

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,24 @@
 
 #include "Quantity.hpp"
 
-void iter(Quantity& n, Quantity& p, std::vector<double>& electric_field, unsigned int t) {
+void iter(Quantity& n, Quantity& p, const Quantity& Cd, const Quantity& Ca, std::vector<double>& electric_field, unsigned int t) {
 	const double K = 1.0; //* == \mu * e / \epsilon
-	const double Cd = 0.0001;
-	const double Ca = 0.0001;
 
-	double qty_integral = n(t, 0) - p(t, 0);
+	double qty_integral = n(t, 0) - p(t, 0) + (Ca(t, 0) - Cd(t, 0));
 	for (int j = 0; j < static_cast<int>(n.get_pos_count()); ++j) {
 		const double dn = 0.5 * (n(t, j + 1) - n(t, j - 1));
 		const double dp = 0.5 * (p(t, j + 1) - p(t, j - 1));
 		const double ddn = 0;//(n(t, j + 1) + n(t, j - 1) - 2 * n(t, j)) / (n.get_pos_step() * n.get_pos_step());
 		const double ddp = 0;//(p(t, j + 1) + p(t, j - 1) - 2 * p(t, j)) / (p.get_pos_step() * p.get_pos_step());
 
-		double atoms_field = 0.0;
-		if (j < static_cast<int>(n.get_pos_count() / 2))
-			atoms_field = Cd * j;
-		else
-			atoms_field = Cd * ((n.get_pos_count() / 2) - 1) - Ca * (j - n.get_pos_count() / 2);
-		const double E = (0.5 * (p(t, 0) + p(t, j) - n(t, 0) - n(t, j))) + qty_integral + atoms_field;
+		const double E = 0.5 * (p(t, 0) + p(t, j) - n(t, 0) - n(t, j) + Cd(t, 0) + Cd(t, j) - Ca(t, 0) - Ca(t, j)) + qty_integral;
+		const double total_charges = p(t, j) - n(t, j) + (Cd(t, j) - Ca(t, j));
 		
 		electric_field.push_back(E * n.get_pos_step());
-		n(t + 1, j) = n(t, j) + 0.5 * n.get_time_step() * (K * (dn * E + (p(t, j) - n(t, j)) * n(t, j)) + ddn);
-		p(t + 1, j) = p(t, j) - 0.5 * p.get_time_step() * (K * (dp * E + (p(t, j) - n(t, j)) * p(t, j)) + ddp);
+		n(t + 1, j) = n(t, j) + 0.5 * n.get_time_step() * (K * (dn * E + total_charges * n(t, j)) + ddn);
+		p(t + 1, j) = p(t, j) - 0.5 * p.get_time_step() * (K * (dp * E + total_charges * p(t, j)) + ddp);
 
-		qty_integral += p(t, j) - n(t, j);
+		qty_integral += total_charges;
 	}
 }
 
@@ -42,13 +36,25 @@ int main() {
 
 	Quantity n{ time_step, time_count, pos_step, pos_count, true };
 	Quantity p{ time_step, time_count, pos_step, pos_count, true };
+	Quantity Cd{ time_step, time_count, pos_step, pos_count };
+	Quantity Ca{ time_step, time_count, pos_step, pos_count };
+
+	const double n_i = 0.001; // initial charge density
 	
 	for (unsigned int j = 0; j < pos_count; ++j) {
-		if (j < pos_count / 2) {
-			n(0, j) = 0.001;
+		if (j < pos_count / 2 - 20) {
+			p(0, j) = n_i;
 		}
-		else {
-			p(0, j) = 0.001;
+		else if (j >= pos_count / 2 + 20) {
+			n(0, j) = n_i;
+		}
+	}
+	for (unsigned int t = 0; t < time_count; ++t) {
+		for (unsigned int j = 0; j < pos_count; ++j) {
+			if (j < pos_count / 2)
+				Ca(t, j) = n_i;
+			else
+				Cd(t, j) = n_i;
 		}
 	}
 
@@ -63,7 +69,7 @@ int main() {
 		n(t, n.get_pos_count()) = 0;
 
 		std::vector<double> electric_field;
-		iter(n, p, electric_field, t);
+		iter(n, p, Cd, Ca, electric_field, t);
 
 		if (t % (time_count / 2000) == 0) {
 			for (double e : electric_field) {
